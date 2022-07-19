3a373ef538e34eba4e039f12870624c0f3f8a806
moshecha <109320893+moshecha@users.noreply.github.com>
2022-07-19 20:03:36 -0300

    subido Moshe chami

---
+++ funciones.py
@@ -0,0 +1,23 @@
+lista = [1, 1.50, 'hola']
+lista1 = 'hola como estas'
+lista3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
+for a in lista:
+    print(a)
+
+for b in lista1:
+    print(b)
+
+print('aaaa', max(lista3))
+print(max(1, 2, 3, 4, 5, 6, 7, 8, 9))
+print(max(1, 1))
+print(lista1[4])
+print(min(True, False))
+
+
+print(len(lista3))
+print(len(lista1))
+print(len(lista))
+print(len('19191919919911'))
+print(max([1, 2, 3, 4, 31]))
+print(len([1, 2, 3, 4]))
+print(lista3)
